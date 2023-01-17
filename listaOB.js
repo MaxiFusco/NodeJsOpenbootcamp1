bb53e206a0a90e2407c79be054c608c7a9008aed
MaxiFusco <maximilianofusco22@gmail.com>
2023-01-16 22:08:28 -0300

    confirmado

---
+++ listaOB.js
@@ -0,0 +1,10 @@
+const nombre = "Maximiliano Fusco";
+const edad = 19;
+const soyDesarrolador = true;
+const fechaNacimiento = new Date(2003, 3 , 22)
+const libroFavorito = {
+    tilulo : "Harry Potter",
+    autor: "J.K Rowling",
+    fecha: 2001
+}
+const lista = [nombre,edad,soyDesarrolador,fechaNacimiento,libroFavorito]
